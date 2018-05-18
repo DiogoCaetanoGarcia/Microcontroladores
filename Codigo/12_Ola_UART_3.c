7d03f808c2c56b882f15c2dd0c45e7b445c2af28
Diogo Caetano Garcia <diogogarcia@unb.br>
2018-05-18 10:29:58 -0300

    Update codigo UART

--- Codigo/12_Ola_UART_3.c
+++ Codigo/12_Ola_UART_3.c
@@ -23,7 +23,7 @@ Conexoes:
 
 */
 
-#include <msp430g2452.h>
+#include <msp430g2553.h>
 #include <legacymsp430.h>
 
 #define AD_IN BIT0
@@ -32,41 +32,33 @@ Conexoes:
 #define TX BIT2
 #define BTN BIT3
 #define LED BIT6
+#define BAUD_9600   0
+#define BAUD_19200  1
+#define BAUD_38400  2
+#define BAUD_56000  3
+#define BAUD_115200 4
+#define BAUD_128000 5
+#define BAUD_256000 6
+#define NUM_BAUDS   7
 
 void Send_Data(volatile unsigned char c);
+void Send_Int(int n);
+void Send_String(char str[]);
+void Init_P1(void);
+void Init_AD(void);
+void Init_UART(unsigned int baud_rate_choice);
 
 int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
-	
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
-	
-	P1OUT |= BTN;
-	P1REN |= BTN;
-	P1DIR &= ~BTN;
-	P1IE = P1IES = BTN;
-	P1OUT &= ~LED;
-	P1DIR |= LED;
 
-	TACCR0 = 10000-1;
-	TACCR1 = TACCR0/2;
-	TACCTL1 = OUTMOD_7;
-	TACTL = TASSEL_2 | ID_0 | MC_1;
+	Init_P1();
+	Init_AD();
+	Init_UART(BAUD_9600);
 	
-	ADC10AE0  = AD_IN;
-	ADC10CTL0 = SREF_0 + ADC10SHT_0 + ADC10IE + ADC10ON;
-	ADC10CTL1 = AD_INCH + SHS_1 + ADC10DIV_0 + ADC10SSEL_3 + CONSEQ_2;
-	ADC10CTL0 |= ENC;
-	
-	P1SEL2 = P1SEL = RX+TX;
-	UCA0CTL0 = 0;
-	UCA0CTL1 = UCSSEL_2;
-	UCA0BR0 = 6;
-	UCA0BR1 = 0;
-	UCA0MCTL = UCBRF_8 + UCOS16;
-
-	_BIS_SR(LPM0_bits+GIE);
+	_BIS_SR(LPM4_bits+GIE);
 	return 0;
 }
 
@@ -74,27 +66,94 @@ interrupt(PORT1_VECTOR) P1_ISR(void)
 {
 	P1OUT |= LED;
 	while((P1IN&BTN)==0);
+	// Manda comecar uma conversao
+	ADC10CTL0 |= ENC + ADC10SC;
+	// Espera a conversao ficar pronta
+	while((ADC10CTL0 & ADC10IFG)==0);
+	Send_String("Leitura A/D = ");
+	Send_Int(ADC10MEM);
+	Send_String("\n");
 	P1IFG &= ~BTN;
 	P1OUT &= ~LED;
-}
-
-interrupt(ADC10_VECTOR) ADC10_ISR(void)
-{
-	static int i = 0;
-	Send_Data(ADC10MEM & 0xff);
-	Send_Data(ADC10MEM >> 8);
-	ADC10CTL0 &= ~ADC10IFG;
-	ADC10CTL0 |= ENC;
-	if(i==49)
-	{
-		P1OUT ^= LED;
-		i=0;
-	}
-	else i++;
 }
 
 void Send_Data(volatile unsigned char c)
 {
 	while((IFG2&UCA0TXIFG)==0);
 	UCA0TXBUF = c;
+}
+
+void Send_Int(int n)
+{
+	int casa, dig;
+	if(n==0)
+	{
+		Send_Data('0');
+		return;
+	}
+	if(n<0)
+	{
+		Send_Data('-');
+		n = -n;
+	}
+	for(casa = 1; casa<=n; casa *= 10);
+	casa /= 10;
+	while(casa>0)
+	{
+		dig = (n/casa);
+		Send_Data(dig+'0');
+		n -= dig*casa;
+		casa /= 10;
+	}
+}
+
+void Send_String(char str[])
+{
+	int i;
+	for(i=0; str[i]!= '\0'; i++)
+		Send_Data(str[i]);
+}
+
+void Init_UART(unsigned int baud_rate_choice)
+{
+	unsigned char BRs[NUM_BAUDS] = {104, 52, 26, 17, 8, 7, 3};
+	unsigned char MCTLs[NUM_BAUDS] = {UCBRF_0+UCBRS_1,
+										UCBRF_0+UCBRS_0,
+										UCBRF_0+UCBRS_0,
+										UCBRF_0+UCBRS_7,
+										UCBRF_0+UCBRS_6,
+										UCBRF_0+UCBRS_7,
+										UCBRF_0+UCBRS_7};
+	if(baud_rate_choice<NUM_BAUDS)
+	{
+		// Habilita os pinos para transmissao serial UART
+		P1SEL2 = P1SEL = RX+TX;
+		// Configura a transmissao serial UART com 8 bits de dados,
+		// sem paridade, comecando pelo bit menos significativo,
+		// e com um bit de STOP
+		UCA0CTL0 = 0;
+		// Escolhe o SMCLK como clock para a UART
+		UCA0CTL1 = UCSSEL_2;
+		// Define a baud rate
+		UCA0BR0 = BRs[baud_rate_choice];
+		UCA0BR1 = 0;
+		UCA0MCTL = MCTLs[baud_rate_choice];
+	}
+}
+
+void Init_P1(void)
+{
+	P1OUT |= BTN;
+	P1REN |= BTN;
+	P1DIR &= ~BTN;
+	P1IE = P1IES = BTN;
+	P1OUT &= ~LED;
+	P1DIR |= LED;
+}
+
+void Init_AD(void)
+{
+	ADC10AE0  = AD_IN;
+	ADC10CTL0 = SREF_0 + ADC10SHT_0 + ADC10ON;
+	ADC10CTL1 = AD_INCH + SHS_0 + ADC10DIV_0 + ADC10SSEL_3 + CONSEQ_0;
 }
